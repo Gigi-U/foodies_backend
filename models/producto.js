93a43a6a76ea736ef5dd193607f952e5bc4242d0
Gigi-U <andanlibres@gmail.com>
2024-06-19 19:07:13 -0300

    quitando metodo duplicado de producto, agregando detalle a update de usuario y agregando postman con usuarios

--- models/producto.js
+++ models/producto.js
@@ -1,12 +1,11 @@
 // Definición de la clase Producto
 class Producto {
-    constructor(id, nombre, descripcion, precio, stock, imagenes = []) {
+    constructor(id, nombre, descripcion, precio, stock) {
         this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
         this.stock = stock;
-        this.imagenes = imagenes; 
     }
 }
 module.exports = Producto;
