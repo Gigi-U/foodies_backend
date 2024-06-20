a52a11a95466031ac8fbe28793f2e900e82b7732
Calgaro89 <cristiancalgaro@gmail.com>
2024-06-20 17:07:19 -0300

    Configuracion de la validacion de login y rutas para unir el front

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -70,6 +70,37 @@ class UsuarioController {
             res.json({ message: 'Usuario eliminado' });
         });
     }
+
+        //Validar inicio de sesion
+        validarInicioSesion(req, res) {
+            const { nombre, password } = req.body;
+        
+            // Buscar usuario por nombre de usuario en la base de datos
+            const sql = 'SELECT * FROM usuario WHERE nombre = ?';
+            db.query(sql, [nombre], (err, usuarios) => {
+                if (err) {
+                    console.error('Error ejecutando la consulta', err);
+                    res.status(500).json({ error: 'Error ejecutando la consulta' });
+                    return;
+                }
+        
+                if (usuarios.length === 0) {
+                    res.status(401).json({ authenticated: false, error: 'Usuario no encontrado' });
+                    return;
+                }
+        
+                const usuario = usuarios[0];
+        
+                // Compara la contraseña ingresada con la contraseña almacenada
+                if (password === usuario.password) {
+                    // Si las contraseñas coinciden, el usuario está validado
+                    res.json({ authenticated: true, message: 'Inicio de sesión exitoso', usuarioId: usuario.id });
+                } else {
+                    // Si las contraseñas no coinciden, devuelve un mensaje de error
+                    res.status(401).json({ authenticated: false, error: 'Contraseña incorrecta' });
+                }
+            });
+        }
 }
 
 const usuarioController = new UsuarioController();
